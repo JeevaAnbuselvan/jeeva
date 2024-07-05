bab3d77669254635658a781f9fecd338d46a108e
JeevaAnbuselvan <jeeva880@gmail.com>
2024-07-05 21:46:35 +0530

    Update application.py

--- application.py
+++ application.py
@@ -84,4 +84,28 @@ def upload_file():
 
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    app.run(debug=True)
+    app.run(host='10.12.41.20', port=5000, debug=True)
+
+######## While executing this script with the template directory in the same directory 
+#[root@localhost TS-Analyzer]# python3 TechSupportApp.py
+# * Serving Flask app 'TechSupportApp'
+# * Debug mode: on
+#WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+# * Running on http://10.12.41.20:5000
+#Press CTRL+C to quit
+# * Restarting with stat
+# * Debugger is active!
+# * Debugger PIN: 451-519-999
+#192.168.230.211 - - [05/Jul/2024 21:25:47] "GET / HTTP/1.1" 200 -
+#192.168.230.211 - - [05/Jul/2024 21:25:47] "GET /favicon.ico HTTP/1.1" 404 -
+#192.168.230.211 - - [05/Jul/2024 21:26
+
+# We have to access from the intranet server using the link http://10.12.41.20:5000
+
+# To disable the FIREWALL in 10.12.41.20
+#[root@localhost TS-Analyzer]# systemctl stop firewalld.service
+#[root@localhost TS-Analyzer]# systemctl status firewalld.service
+#● firewalld.service - firewalld - dynamic firewall daemon
+#  Loaded: loaded (/usr/lib/systemd/system/firewalld.service; disabled; vendor preset: enabled)
+#   Active: inactive (dead)
+#     Docs: man:firewalld(1)
